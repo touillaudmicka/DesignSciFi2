85d5ac1d0bec9246bf722b13b1bb74a28bc54426
Micka Touillaud <m.touillaud@masas.fm>
2018-10-28 16:28:19 -0700

    landing page done

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,15 @@ import Layout from '../components/layout'
 import Image from '../components/image'
 
 const IndexPage = () => (
-  <Layout>
-    <h1>Design what’s next.</h1>
-    <p>Browse Science Fiction and find inspiration.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
+  <div>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Design what’s next</h1>
+        <p>Browse Science Fiction and find inspiration</p>
+        <Link to="/page-2/">Get Early access</Link>
+        </div>
     </div>
-    <Link to="/page-2/">Get early access</Link>
-  </Layout>
+  </div>
 )
 
 export default IndexPage
