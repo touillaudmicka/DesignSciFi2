2fd154cb78490b471bc2efc28c5892c42d078e90
Micka Touillaud <m.touillaud@masas.fm>
2018-10-28 21:55:57 -0700

    card component 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Logo from '../images/logoWhite.png'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
+import Card from '../components/card';
 
 const IndexPage = () => (
   <div>
@@ -13,9 +14,18 @@ const IndexPage = () => (
         <h1>Design what’s next</h1>
         <p>Browse Science Fiction and find inspiration</p>
         <Link to="/page-2/">Get Early access</Link>
+
         </div>
     </div>
+    <div className="Card">
+      <h2>11 collection, more coing</h2>
+      <Card 
+        title="Collections 2" 
+        text="11 collect, more coming"
+        image={require('../images/image1.png')} />
+    </div>
   </div>
+
 )
 
 export default IndexPage
